30d64227e42aab8f44843efc68753805d6dbfd61
Carl Kashnier <Tetum007@gmail.com>
2016-02-24 23:51:51 -0500

    Added Mongo

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ db.collection('urlstorage').find({short: usedURL}).toArray(function(err, results
     res.write(JSON.stringify({"url": "Invalid URL" + (docs+1),"long": checkedURL}));
     res.end();
   } else {
-  res.writeHead(301, { 'location': results.long });
+  res.writeHead(301, { 'location': results[long] });
   res.end();
 }
 })
