bbde4a88f9798b54a162d53609c37f0afb5862a0
Carl Kashnier <Tetum007@gmail.com>
2016-02-24 22:36:53 -0500

    Added Mongo

--- index.js
+++ index.js
@@ -6,64 +6,51 @@ var mongo = require('mongodb')
 var mongoose = require("mongoose")
 
 var server = http.createServer(function (req, res) {
-//Expected input /new/http://www.somewhere.com
-//Expected output http://MyAppNameHere.herokuapp.com/0
-
-//get url
-var usedURL = url.parse(req.url).pathname;
+  var usedURL = url.parse(req.url).pathname;
 //Sanitize URL (this will touch the DB and we want to ensure nothing extra gets in)
-if (usedURL.substring(0,5) == "/new/") {
-var sanitizedURL = sanitize(usedURL.substring(5))
-var checkedURL = checkURL(sanitizedURL)
+  if (usedURL.substring(0,5) == "/new/") {
+      var sanitizedURL = sanitize(usedURL.substring(5))
+      var checkedURL = checkURL(sanitizedURL)
 //This Section Works
-
-if (checkedURL == "Please input valid URL"){
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({"error": checkedURL}));
-  res.end();
-  return
-}
+        if (checkedURL == "Please input valid URL"){
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({"error": checkedURL}));
+            res.end();
+            return
+          }
 //URL entered -- verifyed then add dataset 2MB max size for DB
-mongo.connect(process.env.MONGOLAB_URI,function(err,db){
+      mongo.connect(process.env.MONGOLAB_URI,function(err,db){
 //get number of records & add to DB
-var currentRecord = db.collection('urlstorage').count(function(err, docs){
-db.collection('urlstorage').insert({short: docs+1, long:checkedURL})
-res.writeHead(200, { 'Content-Type': 'application/json' });
-res.write(JSON.stringify({"short": "https://arcane-gorge-62849.herokuapp.com/" + (docs+1),"long": checkedURL}));
-res.end();});
-});
-db.close();
-return
+      var currentRecord = db.collection('urlstorage').count(function(err, docs){
+      db.collection('urlstorage').insert({short: docs+1, long:checkedURL})
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify({"short": "https://arcane-gorge-62849.herokuapp.com/" + (docs+1),"long": checkedURL}));
+      res.end();});
+      });
+      db.close();
+      return
 //After the Else is broken and I am puzzled why
-} else {
+  } else {
 //this works
-  usedURL = parseInt(sanitize(usedURL.substring(1)))
-//This is the part that isn't working
-
-
-
-
-
- mongo.connect(process.env.MONGOLAB_URI,function(err,db){
-var collection = db.collection('urlstorage')
-var intForLU = parseInt(usedURL)
-console.log("#" + intForLU +"#")
-var stuff = collection.findOne(({"short": 1},{long:1, short:0, _id:0}).toArray(function(err, doc) //find if a value exists
-{
-    if(doc) //if it does
-    {
-        console.log(doc); // print out what it sends back
-    }
-    else if(!doc) // if it does not
-    {
+      usedURL = parseInt(sanitize(usedURL.substring(1)))
+      mongo.connect(process.env.MONGOLAB_URI,function(err,db){
+      var collection = db.collection('urlstorage')
+      var intForLU = parseInt(usedURL)
+      console.log("#" + intForLU +"#")
+      var stuff = collection.findOne(({"short": 1},{long:1, short:0, _id:0}).toArray(function(err, doc) //find if a value exists
+      {
+        if(doc) //if it does
+        {
+          console.log(doc); // print out what it sends back
+        }
+        else if(!doc) // if it does not
+        {
         console.log("Not in docs");
-    };
-}));
-
+        };
+      }));
+    });
+}
 });
-
-
-}});
 
 server.listen(process.env.PORT || 8888);
 
